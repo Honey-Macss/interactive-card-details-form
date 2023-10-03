179ee611b92356172613cd7308a2570693735381
Honey-Macss <alasiriepaphras@gmail.com>
2023-10-03 13:27:53 +0100

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className=" min-h-[100svh] ">
+      snajs
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+
+const Footer = () => {
+
+//     <style>
+//     .attribution { font-size: 11px; text-align: center; }
+//     .attribution a { color: hsl(228, 45%, 44%); }
+//   </style>
+  return (
+    <div className="">
+    <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="blank">Frontend Mentor</a>. </p>
+    <p>Coded by <a href="https://github.com/Honey-Macss" target="blank">Honey Macss</a>.</p>
+  </div>
+  )
+}
+
+export default Footer
